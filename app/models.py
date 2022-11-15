9bd7f74735ef9c41dd985d5488b03db8fe131cda
JLeandroJM <jose.machaca.s@utec.edu.pe>
2022-11-15 12:04:57 -0500

    modelos hechos

--- app/models.py
+++ app/models.py
@@ -1,48 +1,72 @@
 from app import db
+from datetime import datetime
+
+
+
+
+
+class Local(db.Model):
+
+    adress = db.Column(db.String(120), primary_key=True)
+    reference = db.Column(db.String(64), index=True, unique=True)
+    open = db.Column(db.Integer(),nullable= False)
+    close = db.Column(db.Integer(),nullable= False)
+
+
+
+class Bebidas(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    price = db.Column(db.Float(),nullable = False)
+    image = db.Column(db.String(300), nullable = False, unique = True)
+
+class Pastas(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    price = db.Column(db.Float(),nullable = False)
+    image = db.Column(db.String(300), nullable = False, unique = True)
+
+
+
 
 class User(db.Model):
 
     email = db.Column(db.String(120), primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password = db.Column(db.String(128),nullable=False)
-    phone = db.Column(db.String(9),nullable =False)
-    adress =db.Column(db.String(200),nullable=False)
+    phone = db.Column(db.String(9),nullable =False, unique =True)
+    adress =db.Column(db.String(200),nullable=False, unique = True)
+    pedidos = db.relationship('Pedidos', backref='user', lazy='dynamic')
 
-class Locales(db.Model):
 
-    adress = db.Column(db.String(120), primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    opens = db.Column(db.Integer(),nullable= False)
-    clases = db.Column(db.Integer(),nullable= False)
+class Pedidos(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)    
+    product = db.Column(db.String(30), nullable=False)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    amount = db.Column(db.Float(),nullable = False)
+    user_email = db.Column(db.String(120), db.ForeignKey('user.email'),nullable = False)
+    
+    
+
+
+association_table = db.Table('Relation',
+db.Column('pizza_id', db.Integer, db.ForeignKey('pizzas.id'), primary_key=True),
+db.Column('ingredient_id', db.Integer, db.ForeignKey('ingredients.id'), primary_key=True)
+)
+
 
 class Pizzas(db.Model):
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
     size = db.Column(db.String(15),nullable = False)
-    price = db.Column(db.Integer(),nullable = False)
+    price = db.Column(db.Float(),nullable = False)
+    image = db.Column(db.String(300), nullable = False, unique = True)
 
-class Bebidas(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(30), nullable=False)
-    price = db.Column(db.Integer(),nullable = False)
-
-class Pastas(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(30), nullable=False)
-    price = db.Column(db.Integer(),nullable = False)
 
 class Ingredients(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
-    id_producto = db.Column(db.Integer(), db.ForeignKey('pizzas.id'), nullable=False)  
-    pizzas = db.relationship("Pizzas", backref="ingredients") 
-
-class Pedidos(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    id_usuario = db.Column(db.String(50), db.ForeignKey('usuario.email'), nullable=False)
-    product = db.Column(db.String(30), nullable=False)
-    amount = db.Column(db.Integer(),nullable = False)
-    usuario = db.relationship("Usuario", backref="pedidos")
-    time = db.Column(db.String(30), nullable=False)
+    price = db.Column(db.Float(),nullable = False)
+    image = db.Column(db.String(300), nullable = False, unique = True)
 
